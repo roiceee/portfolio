9a1753775c6912fd6046232b42d664e7288bfd81
jroiceee <jroicealdeza@gmail.com>
2023-05-25 21:20:51 +0800

    add alt text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import Card from "@/components/techstack-card/card";
-import html from "public/html.svg";
-import css from "public/css-3.svg";
-import js from "public/javascript.svg";
-import tailwind from "public/tailwindcss-icon.svg";
-import react from "public/react.svg";
-import nextjs from "public/nextjs-icon.svg";
-import firebase from "public/firebase.svg";
-import java from "public/java.svg";
-import spring from "public/spring-icon.svg";
-import mysql from "public/mysql.svg";
-import postgres from "public/postgresql.svg";
-import figma from "public/figma.svg";
+import TechstackCard from "@/components/techstack-card/techstack-card";
+import html from "public/html.png";
+import css from "public/css-3.png";
+import js from "public/javascript.png";
+import tailwind from "public/tailwindcss-icon.png";
+import react from "public/react.png";
+import nextjs from "public/nextjs-icon.png";
+import firebase from "public/firebase.png";
+import java from "public/java.png";
+import spring from "public/spring-icon.png";
+import mysql from "public/mysql.png";
+import postgres from "public/postgresql.png";
+import figma from "public/figma.png";
 
 export default function Home() {
   return (
@@ -28,19 +28,22 @@ export default function Home() {
       <section className="py-2">
         <h3>Tech Stack</h3>
         <div className="grid grid-cols-3 lg:grid-cols-6 gap-3 lg:gap-6">
-          <Card imageSrc={html} label="HTML" />
-          <Card imageSrc={css} label="CSS" />
-          <Card imageSrc={tailwind} label="Tailwind CSS" />
-          <Card imageSrc={js} label="JavaScript" />
-          <Card imageSrc={figma} label="Figma"/>
-          <Card imageSrc={react} label="React" />
-          <Card imageSrc={nextjs} label="NextJs" />
-          <Card imageSrc={firebase} label="Firebase" />
-          <Card imageSrc={java} label="Java" />
-          <Card imageSrc={spring} label="Spring" />
-          <Card imageSrc={mysql} label="MySQL" />
-          <Card imageSrc={postgres} label="PostgreSQL" />
+          <TechstackCard imageSrc={html} label="HTML" altText="HTML"/>
+          <TechstackCard imageSrc={css} label="CSS" altText="CSS"/>
+          <TechstackCard imageSrc={tailwind} label="Tailwind CSS" altText="Tailwind CSS"/>
+          <TechstackCard imageSrc={js} label="JavaScript" altText="JavaScript"/>
+          <TechstackCard imageSrc={figma} label="Figma" altText="Figma"/>
+          <TechstackCard imageSrc={react} label="React" altText="React"/>
+          <TechstackCard imageSrc={nextjs} label="NextJs" altText="NextJs"/>
+          <TechstackCard imageSrc={firebase} label="Firebase" altText="Firebase"/>
+          <TechstackCard imageSrc={java} label="Java" altText="Java"/>
+          <TechstackCard imageSrc={spring} label="Spring" altText="Spring"/>
+          <TechstackCard imageSrc={mysql} label="MySQL" altText="MySQL"/>
+          <TechstackCard imageSrc={postgres} label="PostgreSQL" altText="PostgreSQL"/>
         </div>
+      </section>
+      <section className="py-2">
+        <h3>Projects</h3>
       </section>
     </main>
   );
