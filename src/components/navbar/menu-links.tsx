973248ea9188019393cc049537fd6c9c4779eb04
jroiceee <jroicealdeza@gmail.com>
2023-05-25 15:55:42 +0800

    add navbar

---
+++ src/components/navbar/menu-links.tsx
@@ -0,0 +1,17 @@
+function MenuItems() {
+  return (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Projects</a>
+      </li>
+      <li>
+        <a>Contact me</a>
+      </li>
+    </>
+  );
+}
+
+export default MenuItems;
