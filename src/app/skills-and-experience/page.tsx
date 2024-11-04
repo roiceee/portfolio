3aabf6094a69e7cc2fc16b007726db4f6232cca3
John Roice Aldeza <jroicealdeza@gmail.com>
2024-11-04 13:53:58 +0800

    Add initial skills and experience page component

---
+++ src/app/skills-and-experience/page.tsx
@@ -0,0 +1,15 @@
+import Tabs from "@/components/skills-and-exp/tabs";
+
+export default function Page() {
+
+  
+
+  return (
+    <div>
+      <section className="prose my-10">
+        <h1>Skills & Experience</h1>
+      </section>
+      <Tabs />
+    </div>
+  );
+}
