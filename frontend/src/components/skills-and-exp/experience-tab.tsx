8d67338de5e22bfd1111effad03e9640bc7804a6
John Roice Aldeza <jroicealdeza@gmail.com>
2025-06-17 23:36:34 +0800

    Update experience and projects data in ExperienceTab and ProjectsTab components

--- frontend/src/components/skills-and-exp/experience-tab.tsx
+++ frontend/src/components/skills-and-exp/experience-tab.tsx
@@ -1,23 +1,34 @@
 import Link from "next/link";
 
-export default function ExperienceTab(
-  { isPreview = false }: { isPreview?: boolean }
-) {
+export default function ExperienceTab({
+  isPreview = false,
+}: {
+  isPreview?: boolean;
+}) {
   const experienceData = [
     {
-      title: "Software Developer",
-      company: "Imperate Realty",
-      duration: "August 2024 - Present",
+      title: "Fullstack Developer Intern",
+      company: "Symph",
+      duration: "March 2025 - July 2025",
       description: [
-        "Deployed applications using AWS, utilizing services like EC2, RDS, S3, and other AWS solutions.",
-        "Developed the website using Next.js for efficient, server-side rendering and performance.",
-        "Translated Figma designs into responsive and interactive web interfaces.",
-        "Set up a headless CMS on EC2 instances to manage content effectively.",
+        "Improved the codebase to follow a more maintainable structure and best practices.",
+        "Created new features for an AI-powered project.",
+        "Optimized network payload to enhance performance and reduce latency.",
       ],
     },
     {
-      title: "Freelance Developer",
-      duration: "June 2023 - July 2024",
+      title: "Software Developer",
+      company: "Imperate Realty",
+      duration: "August 2024 - February 2025",
+      description: [
+        "Deployed applications using AWS, utilizing services like EC2, RDS, S3, and other AWS solutions.",
+        "Developed the website using Next.js for efficient, server-side rendering and performance.",
+        "Set up services on EC2 instances to manage content effectively.",
+      ],
+    },
+    {
+      title: "Freelance Fullstack Web Developer",
+      duration: "June 2023 - Present",
       description: [
         "Created web applications to interface with Arduino devices for remote monitoring and control.",
         "Developed an e-commerce website for a business, including payment integration and inventory management.",
@@ -27,7 +38,9 @@ export default function ExperienceTab(
     // Add more experiences here if needed
   ];
 
-  const displayedExperiences = isPreview ? experienceData.slice(0, 3) : experienceData;
+  const displayedExperiences = isPreview
+    ? experienceData.slice(0, 3)
+    : experienceData;
 
   return (
     <div>
@@ -48,7 +61,10 @@ export default function ExperienceTab(
       ))}
       {isPreview && (
         <div className="text-center">
-          <Link href="/skills-and-experience#experience" className="underline font-bold">
+          <Link
+            href="/skills-and-experience#experience"
+            className="underline font-bold"
+          >
             See more
           </Link>
         </div>
--- frontend/src/components/skills-and-exp/projects-tab.tsx
+++ frontend/src/components/skills-and-exp/projects-tab.tsx
@@ -1,91 +1,117 @@
 export default function ProjectsTab() {
-    const projectsData = [
-      {
-        title: "DEVCON Mindanao Summit Platform",
-        description:
-          "Event platform which allows users to access event-related resources and participate in activities through the app.",
-        role: "Contributor",
-        techStack: ["Pocketbase", "TypeScript", "React", "Node.js"],
-        achievements: [
-          "Designed and implemented a comprehensive database schema using Pocketbase, ensuring robust data management.",
-          "Optimized frontend queries in TypeScript for efficient data retrieval, enhancing user experience.",
-          "Collaborated with a multidisciplinary team to integrate diverse features seamlessly.",
-        ],
-        link: "https://github.com/nedpals/summit-platform",
-      },
-      {
-        title: "Riffrant",
-        description: "Social media application to express your opinions.",
-        role: "Fullstack Developer",
-        techStack: ["MongoDB", "React", "Node.js"],
-        achievements: [
-          "Implemented rate-limiting API to prevent spam and enhance security.",
-          "Integrated MongoDB for scalable and efficient data storage.",
-          "Developed responsive user interfaces using React.",
-        ],
-        link: "https://github.com/roiceee/riffrant",
-        liveLink: "https://riffrant.vercel.app",
-      },
-      {
-        title: "Phrase API",
-        description:
-          "Platform to share and look up text-related data, with REST API endpoints for easy integration.",
-        role: "Fullstack Developer",
-        techStack: ["MongoDB", "Express", "Node.js"],
-        achievements: [
-          "Implemented secure user authentication and authorization.",
-          "Designed and implemented database schema for efficient data storage.",
-          "Created documentation for end users to navigate and use the application effectively.",
-        ],
-        link: "https://github.com/roiceee/phrase-api-server",
-        liveLink: "https://phraseapi.vercel.app",
-      },
-    ];
-  
-    return (
-      <div>
-        <h1>Projects</h1>
-  
-        {projectsData.map((project, index) => (
-          <div key={index} className="mb-8">
-            <h3>{project.title}</h3>
-            <p className="mb-1">{project.description}</p>
-            <p className="font-semibold">Role: {project.role}</p>
-            <div className="flex flex-wrap gap-2 mt-2">
-              {project.techStack.map((tech, i) => (
-                <span
-                  key={i}
-                  className="bg-gray-200 text-gray-800 px-2 py-1 rounded text-sm"
-                >
-                  {tech}
-                </span>
-              ))}
-            </div>
-            <ul className="list-disc list-inside mt-2">
-              {project.achievements.map((achievement, i) => (
-                <li key={i}>{achievement}</li>
-              ))}
-            </ul>
-            <div className="mt-4">
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
-                <button className="btn btn-primary">View Code</button>
-              </a>
-              {project.liveLink && (
-                <a
-                  href={project.liveLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="ml-2"
-                >
-                  <button className="btn btn-secondary">Live View</button>
-                </a>
-              )}
-               {index < projectsData.length - 1 && <hr />}
-            </div>
+  const projectsData = [
+    {
+      title: "Digital Preflight Checklist System",
+      description:
+        "A PWA built to streamline aviation preflight procedures for pilots and mechanics.",
+      role: "Fullstack Developer",
+      techStack: ["React", "Supabase", "TypeScript", "Vite"],
+      achievements: [
+        "Developed a PWA using Vite and React for seamless user experience.",
+        "Integrated Supabase Cloud for backend services and authentication.",
+        "Deployed frontend on Vercel and backend on Supabase Cloud with CI/CD.",
+      ],
+      link: "https://github.com/roiceee/aerocheck",
+      liveLink: "https://aerocheck.vercel.app",
+    },
+    {
+      title: "Sniplate (Chrome Extension)",
+      description:
+        "A Chrome extension that lets users snip text from any screen area and instantly get translations.",
+      role: "Fullstack Developer",
+      techStack: ["NestJS", "OCR", "Google Translate API"],
+      achievements: [
+        "Enables users to snip text from any part of the screen and get instant translations.",
+        "Backend built with NestJS, utilizing the Google Translate API for accurate and fast translations.",
+        "Designed a seamless user experience integrated directly into Chrome.",
+      ],
+      liveLink:
+        "https://chromewebstore.google.com/detail/sniplate/ilopiodlfcmpfbhnljnhggfbepgimpin",
+    },
+    {
+      title: "DEVCON Mindanao Summit Platform",
+      description:
+        "Event platform which allows users to access event-related resources and participate in activities through the app.",
+      role: "Contributor",
+      techStack: ["Pocketbase", "TypeScript", "React", "Node.js"],
+      achievements: [
+        "Designed and implemented a comprehensive database schema using Pocketbase, ensuring robust data management.",
+        "Optimized frontend queries in TypeScript for efficient data retrieval, enhancing user experience.",
+        "Collaborated with a multidisciplinary team to integrate diverse features seamlessly.",
+      ],
+      link: "https://github.com/nedpals/summit-platform",
+    },
+    {
+      title: "Riffrant",
+      description: "Social media application to express your opinions.",
+      role: "Fullstack Developer",
+      techStack: ["MongoDB", "React", "Node.js"],
+      achievements: [
+        "Implemented rate-limiting API to prevent spam and enhance security.",
+        "Integrated MongoDB for scalable and efficient data storage.",
+        "Developed responsive user interfaces using React.",
+      ],
+      link: "https://github.com/roiceee/riffrant",
+      liveLink: "https://riffrant.vercel.app",
+    },
+    // {
+    //   title: "Phrase API",
+    //   description:
+    //     "Platform to share and look up text-related data, with REST API endpoints for easy integration.",
+    //   role: "Fullstack Developer",
+    //   techStack: ["MongoDB", "Express", "Node.js"],
+    //   achievements: [
+    //     "Implemented secure user authentication and authorization.",
+    //     "Designed and implemented database schema for efficient data storage.",
+    //     "Created documentation for end users to navigate and use the application effectively.",
+    //   ],
+    //   link: "https://github.com/roiceee/phrase-api-server",
+    //   liveLink: "https://phraseapi.vercel.app",
+    // },
+  ];
+
+  return (
+    <div>
+      <h1>Projects</h1>
+
+      {projectsData.map((project, index) => (
+        <div key={index} className="mb-8">
+          <h3>{project.title}</h3>
+          <p className="mb-1">{project.description}</p>
+          <p className="font-semibold">Role: {project.role}</p>
+          <div className="flex flex-wrap gap-2 mt-2">
+            {project.techStack.map((tech, i) => (
+              <span
+                key={i}
+                className="bg-gray-200 text-gray-800 px-2 py-1 rounded text-sm"
+              >
+                {tech}
+              </span>
+            ))}
           </div>
-          
-        ))}
-      </div>
-    );
-  }
-  
+          <ul className="list-disc list-inside mt-2">
+            {project.achievements.map((achievement, i) => (
+              <li key={i}>{achievement}</li>
+            ))}
+          </ul>
+          <div className="mt-4">
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <button className="btn btn-primary">View Code</button>
+            </a>
+            {project.liveLink && (
+              <a
+                href={project.liveLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-2"
+              >
+                <button className="btn btn-secondary">Live View</button>
+              </a>
+            )}
+            {index < projectsData.length - 1 && <hr />}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
